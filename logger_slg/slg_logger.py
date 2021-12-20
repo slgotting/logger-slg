4ccd12d6d7a8785be1991314bc93163864a27e59
slgotting <sgotting21@gmail.com>
2021-12-20 18:09:47 -0500

    update version

--- logger_slg/slg_logger.py
+++ logger_slg/slg_logger.py
@@ -8,13 +8,12 @@ def init_logger(
     name = 'default_logger',
     log_path = '/var/log/slg/default_location.log',
     log_level = logging.INFO,
-    formatter = Formatter('%(asctime)s  | %(levelname)-8s | Line %(lineno)-4s | %(pathname)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S'),
+    formatter_str = '%(asctime)s | %(levelname)-8s | Line %(lineno)-4s | %(pathname)s | %(message)s'
 ) -> logging.Logger:
     '''
     It's recommended to set name = __name__ as the name parameter when calling this function in order to get the logger name to be the same as the file's name.
 
     It's also advised to set the log_path to "/var/log/slg/{__file__.split("/")[-1]}.log" when calling from a script so you don't log to a default file but also one relevant to the script.
-
     '''
 
     try:
@@ -22,8 +21,12 @@ def init_logger(
         file_handler = RotatingFileHandler(log_path, maxBytes=10000000, backupCount=5)
         stream_handler = StreamHandler()
 
-        file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
+        file_formatter = Formatter(formatter_str, datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(file_formatter)
+
+        # we also log to the stream where the logs are saved
+        stream_formatter = Formatter(f'------- Log message saved to {log_path} ------- \n' + formatter_str, datefmt='%Y-%m-%d %H:%M:%S')
+        stream_handler.setFormatter(stream_formatter)
 
         stream_handler.setLevel(log_level)
         logger.addHandler(stream_handler)
